37c8d9b5023047bd1b3ebea82bd0822cd3743f3c
Austromo <moritz.gruber.linz@gmail.com>
2024-05-21 14:53:40 +0200

    feat: second task - add color marks

--- todo/app/src/main/java/at/htl/todo/ui/layout/TodosList.kt
+++ todo/app/src/main/java/at/htl/todo/ui/layout/TodosList.kt
@@ -1,5 +1,6 @@
 package at.htl.todo.ui.layout
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -11,6 +12,7 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Info
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
@@ -21,6 +23,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import at.htl.todo.model.Model
@@ -48,11 +51,17 @@ fun TodosList(model: Model, modifier: Modifier = Modifier, store: ModelStore?) {
 
 @Composable
 fun TodoRow(todo: Todo, store: ModelStore?) {
+    val cardColor = if (todo.completed) Color(0xFFCEFAD0) else Color(0xFFFFE8E7)
+
     Card(
         modifier = Modifier
             .fillMaxWidth()
             .padding(8.dp)
-            .clip(RoundedCornerShape(16.dp))
+            .clip(RoundedCornerShape(5.dp)),
+        colors = CardDefaults.cardColors(
+            containerColor = cardColor
+        ),
+        border = BorderStroke(1.dp, Color.Black),
     ) {
         Row(
             modifier = Modifier
